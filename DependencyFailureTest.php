d20738fafbc8b4f0cff2ebf481edd2672edd5512
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2012-09-19 22:54:32 +0700

    Added first test with PHPUnit

--- DependencyFailureTest.php
+++ DependencyFailureTest.php
@@ -3,14 +3,7 @@ class DependencyFailureTest extends PHPUnit_Framework_TestCase
 {
     public function testOne()
     {
-        $this->assertTrue(FALSE);
-    }
- 
-    /**
-     * @depends testOne
-     */
-    public function testTwo()
-    {
-    }
+        $this->assertTrue(TRUE);
+    }    
 }
 ?>
